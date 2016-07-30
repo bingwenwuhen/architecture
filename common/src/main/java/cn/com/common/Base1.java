35bef75a77133c2991e24a6be1647ff06e7ca944
bingwenwuhen <bingwenwuhen@163.com>
2016-07-30 08:50:54 +0800

    开始工程搭建

---
+++ common/src/main/java/cn/com/common/Base1.java
@@ -0,0 +1,11 @@
+package cn.com.common;
+
+/**
+ * Created by Administrator on 2016/7/28.
+ */
+public class Base1 {
+
+    public void base1() {
+        System.out.println("now in base1");
+    }
+}
---
+++ customermgr/src/main/java/cn/com/vo/CustomerModel.java
@@ -0,0 +1,7 @@
+package cn.com.vo;
+
+/**
+ * Created by Administrator on 2016/7/30.
+ */
+public class CustomerModel {
+}
