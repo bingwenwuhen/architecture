2be7b30cb31188427bc070be8d83182fa549344c
xiaxuan <xiaxuan@haizhi.com>
2016-08-07 16:37:56 +0800

    登录研发

--- front/src/main/java/cn/com/controllers/IndexController.java
+++ front/src/main/java/cn/com/controllers/IndexController.java
@@ -1,7 +1,9 @@
 package cn.com.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by Administrator on 2016/8/7.
@@ -10,7 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class IndexController {
 
-    public String toIndex() {
+    @RequestMapping(value = "/toIndex", method = RequestMethod.GET)
+    public String toIndex(Model model) {
         return "";
     }
 
